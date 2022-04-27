753beb09f5b62499aed5c48623b7c73ffc563787
Moosa Hassan <moosa7021@gmail.com>
2022-04-28 08:55:18 +1000

    testing routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import Main from "./Main";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>This is a testing ground so I know what to add in my portfolio page. Ignore this site.</p>
-      </header>
+      <Main/>
     </div>
   );
 }
